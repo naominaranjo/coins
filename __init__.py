a2892997f3f9deb97de0edc7a1d89c48c5f92e9c
turtlelazy <ishraqmahid@gmail.com>
2022-03-03 08:25:46 -0500

    project initialization

---
+++ __init__.py
@@ -0,0 +1,14 @@
+
+from flask import Flask, render_template
+import urllib.request
+import json
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return ""
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
