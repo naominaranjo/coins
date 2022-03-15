3df1cf296f53e5f280ddba38c0fe668a82a09c77
Oscar <oscarwang22396@gmail.com>
2022-03-15 08:01:17 -0400

    no merge err pls

--- __init__.py
+++ __init__.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def main():
     return render_template("beep.html")
 
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
+
