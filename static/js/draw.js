6dfe1f4b6a7d58b4553108a98c27543390598ba8
Shriya <>
2022-03-18 23:43:25 -0400

    rectangle works

--- static/js/draw.js
+++ static/js/draw.js
@@ -8,6 +8,10 @@ let color;
 let penWidth;
 let eraserWidth;
 let erase = false;
+let initialize = false;
+let corner1 = true;
+let rectDone = false;
+let x1, x2, y1, y2;
 // This is the flag that we are going to use to
 // trigger drawing
 let mode = "paint";
@@ -23,7 +27,7 @@ function getPosition(event) {
     coord.x = event.clientX - rect.left;
     coord.y = event.clientY - rect.top;
 }
-  
+
 // The following functions toggle the flag to start
 // and stop drawing
 function mouseDown(event) {
@@ -36,11 +40,36 @@ function mouseDown(event) {
     erase = true;
     getPosition(event);
     }
-    if (mode == "rect"){
-    getPosition(event);
-    console.log("mode correct - rect");
-    }
 }
+
+function mouseClick(){
+    if (mode == "rect"){
+      ctx.beginPath();
+      ctx.lineWidth = 5;
+      ctx.lineCap = "round";
+      ctx.strokeStyle = "black";
+      if (!corner1){
+        getPosition(event);
+        x2 = coord.x;
+        y2 = coord.y;
+        ctx.rect(x1,y1,x2-x1,y2-y1);
+        ctx.stroke();
+        corner1 = true;
+        console.log("cor2");
+        return;
+      }
+      if (corner1){
+        getPosition(event);
+        x1 = coord.x;
+        y1 = coord.y;
+        corner1 = false;
+        console.log("cor1");
+        return;
+      }
+
+  }
+}
+
 function mouseUp() {
     erase = false;
     paint = false;
@@ -77,21 +106,9 @@ function sketch(event) {
     ctx.stroke();
     }
 
-    if (mode == "rect"){
-    ctx.beginPath();
-    ctx.lineWidth = eraserWidth;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = "black";
-    ctx.rect(coord.x, coord.y, coord.x + 50, coord.y + 50);
-    ctx.stroke();
-
-    // ctx.moveTo(coord.x, coord.y);
-    // getPosition(event);
-    // ctx.lineTo(coord.x, coord.y);
-    // ctx.stroke();
-    }
-
 }
+
+
 
 // clears the canvas
 function clear(e) {
@@ -123,4 +140,4 @@ function penCol(){
     console.log(color);
 }
 
-export { getPosition, mouseDown, mouseUp, sketch, clear, penOn, eraseOn, rectOn, penCol };
+export { getPosition, mouseDown, mouseUp, sketch, clear, penOn, eraseOn, rectOn, penCol, mouseClick };
